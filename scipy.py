6561a785d81db8d7a1345b951445f6213d3671a7
wkmvstak <sspes.66@gmail.com>
2017-06-08 09:56:05 +0200

    trabajo sobre variables
    Añadidas instrucciones para poder extraer el valor de las variables

--- scipy.py
+++ scipy.py
@@ -1,6 +1,15 @@
+import matplotlib.pyplot as plt
 import numpy as np
 print "Medicion de tiempos de ejecucion para algoritmo de AEDII"
-toread=open(input.txt)
-toread.readline():
+toread=open("input.txt")
 array=toread.readlines()
-
+print array
+fstline=array[0].split()
+tramos=int(fstline[0])
+variaciones=int(fstline[1])
+numElem=[] #Num de elementos (Eje x)
+tiempos=[] #Medicion de tiempos (Eje y)
+titletrab=[] #Numero de trabajadores (Titulo)
+numtrab=0
+numw=0
+#Proximamente el while
